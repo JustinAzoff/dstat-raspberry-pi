32514f53fa9093a0fe18acc73f1a0cd19b2c31cb
Justin Azoff <justin@bouncybouncy.net>
2012-09-21 18:17:37 -0400

    new repo

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ dstat-raspberry-pi
 ==================
 
 dstat plugins for Raspberry Pi
+
+Copy to one of
+* ~/.dstat
+* /usr/share/dstat/
+* /usr/local/share/dstat/
+
+and run something like 
+
+    dstat --time -a --cpufreq --rpi-thermal 5
+
---
+++ dstat_rpi_thermal.py
@@ -0,0 +1,19 @@
+### Author: Justin Azoff <justin@bouncybouncy.net>
+
+class dstat_plugin(dstat):
+    TEMP_FILE = "/sys/devices/virtual/thermal/thermal_zone0/temp"
+    def __init__(self):
+        self.name = 'Temp'
+        self.type = 'f'
+        self.width = 5
+        self.scale = 5
+        self.vars = self.nick = ['temp']
+        if not os.path.exists(self.TEMP_FILE):
+                raise Exception, 'Needs newer Raspberry Pi firmware'
+
+    def extract(self):
+        with open(self.TEMP_FILE) as f:
+            temp = int(f.read())/1000.0
+        self.val['temp'] = temp
+
+# vim:ts=4:sw=4:et
